f6d6e50c4214d40f37ebd4c3469f78c9505485d7
Thierry <thierry2003email.fr@gmail.com>
2026-03-27 12:45:30 +0100

    tools added

---
+++ LLM/tool.py
@@ -0,0 +1,30 @@
+import math
+
+def calculDistance(bbox1, bbox2):
+    """
+    Calcule la distance entre les centres de deux bounding boxes.
+    bbox = {x_min, y_min, x_max, y_max}
+    """
+    x1_center = (bbox1["x_min"] + bbox1["x_max"]) / 2
+    y1_center = (bbox1["y_min"] + bbox1["y_max"]) / 2
+
+    x2_center = (bbox2["x_min"] + bbox2["x_max"]) / 2
+    y2_center = (bbox2["y_min"] + bbox2["y_max"]) / 2
+
+    distance = math.sqrt((x2_center - x1_center)**2 + (y2_center - y1_center)**2)
+    return distance
+
+
+CalculDistance_tool = {
+    "name": "calculDistance",
+    "description": "Calcule la distance entre deux objets détectés à partir de leurs bounding boxes.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "bbox1": {"type": "object"},
+            "bbox2": {"type": "object"}
+        },
+        "required": ["bbox1", "bbox2"]
+    },
+    "function": calculDistance
+}
